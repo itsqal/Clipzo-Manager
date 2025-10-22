73641f7b4b591eafd64994a264ffb90e9a3af9f3
user.email <rickoaiko112@gmail.com>
2025-10-22 16:30:55 +0700

    SCRUM-19 Selesaikan fitur input sampai service selector

---
+++ app/Livewire/Admin/ServiceSelector.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class ServiceSelector extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.service-selector');
+    }
+}
